ef597691a3b360f3c3471c4c6652ce7d4ff05eaf
Python Blue <29385655+PythonBlue@users.noreply.github.com>
2025-08-04 19:17:54 -0400

    Beginnings of tone parameter support
    For the sake of updates to this project, and to test my ability to commit to someone else's repo, began work on attempting to to fill up the Tone tab in the VST via showing controls for the wave group and the waveform selection per tone (no names for now: only numbers). The two parameters are not editable at this time due to needing to find the time to implement a whole automatic working SysEx request for tones, but if nothing else, it can currently provide some good reference for sound design back in the day.

--- Source/ui/EditToneTab.cpp
+++ Source/ui/EditToneTab.cpp
@@ -10,17 +10,91 @@
 
 #include <JuceHeader.h>
 #include "EditToneTab.h"
+#include "../dataStructures.h"
 
 //==============================================================================
 EditToneTab::EditToneTab(Jv880_juceAudioProcessor& p) :
   audioProcessor (p)
 {
+    addAndMakeVisible(toneSelectionLabel);
+    toneSelectionLabel.setText ("Wave Group", juce::dontSendNotification);
+    toneSelectionLabel.attachToComponent (&toneSelectionComboBox, true);
+    addAndMakeVisible (toneSelectionComboBox);
+    toneSelectionComboBox.addListener (this);
+    toneSelectionComboBox.addItem ("Tone A", 1);
+    toneSelectionComboBox.addItem ("Tone B", 2);
+    toneSelectionComboBox.addItem ("Tone C", 3);
+    toneSelectionComboBox.addItem ("Tone D", 4);
+    
+    
+    addAndMakeVisible(waveGroupLabel);
+    waveGroupLabel.setText ("Wave Group", juce::dontSendNotification);
+    waveGroupLabel.attachToComponent (&waveGroupComboBox, true);
+    addAndMakeVisible (waveGroupComboBox);
+    waveGroupComboBox.addListener (this);
+    waveGroupComboBox.addItem ("Internal", 1);
+    waveGroupComboBox.addItem ("Expansion", 2);
+    
+    addAndMakeVisible(waveformLabel);
+    waveformLabel.setText ("Waveform", juce::dontSendNotification);
+    waveformLabel.attachToComponent (&waveformSlider, true);
+    addAndMakeVisible (waveformSlider);
+    waveformSlider.setRange (0, 255);
+    waveformSlider.addListener (this);
 }
 
 EditToneTab::~EditToneTab()
 {
 }
 
+void EditToneTab::updateValues()
+{
+    Patch* patch = (Patch*) audioProcessor.status.patch;
+    Tone tone = patch->tones[toneSelectionComboBox.getSelectedItemIndex()];
+    waveGroupComboBox.setSelectedItemIndex((tone.flags & 0x3), juce::dontSendNotification);
+    waveformSlider.setValue(tone.waveNumber & 0xff, juce::dontSendNotification);
+}
+
+void EditToneTab::visibilityChanged()
+{
+    updateValues();
+}
+
 void EditToneTab::resized()
 {
+    auto top = 30;
+    auto sliderLeft1 = 120;
+    auto width = getWidth() / 2 - sliderLeft1 - 20;
+    auto height = 32;
+    auto groupMargin = 24;
+    toneSelectionComboBox.setBounds(sliderLeft1, top + height * 0 + groupMargin * 0, width, height);
+    waveGroupComboBox.setBounds(sliderLeft1, top + height * 2 + groupMargin * 0, width, height);
+    waveformSlider.setBounds(sliderLeft1, top + height * 3 + groupMargin * 0, width, height);
+}
+
+void EditToneTab::sliderValueChanged (juce::Slider* slider)
+{
+    if (slider == &waveformSlider)
+    {
+        uint8_t toneNum = toneSelectionComboBox.getSelectedItemIndex() - 1;
+        uint32_t address = 0x1A + 0x01 + (0x54 * toneNum);
+        uint8_t value = waveformSlider.getValue();
+        audioProcessor.sendSysexParamChange(address, value);
+    }
+}
+
+void EditToneTab::buttonClicked (juce::Button* button)
+{
+}
+
+void EditToneTab::buttonStateChanged (juce::Button* button)
+{
+}
+
+void EditToneTab::comboBoxChanged (juce::ComboBox* button)
+{
+    if (button == &toneSelectionComboBox)
+    {
+        updateValues();
+    }
 }
--- Source/ui/EditToneTab.h
+++ Source/ui/EditToneTab.h
@@ -16,16 +16,29 @@
 //==============================================================================
 /*
 */
-class EditToneTab  : public juce::Component
+class EditToneTab  : public juce::Component, public juce::Slider::Listener, public juce::Button::Listener, public juce::ComboBox::Listener
 {
 public:
     EditToneTab(Jv880_juceAudioProcessor&);
     ~EditToneTab() override;
+    
+    void updateValues();
 
+    void visibilityChanged() override;
     void resized() override;
+    void sliderValueChanged (juce::Slider*) override;
+    void buttonClicked (juce::Button*) override;
+    void buttonStateChanged (juce::Button*) override;
+    void comboBoxChanged (juce::ComboBox*) override;
 
 private:
     Jv880_juceAudioProcessor& audioProcessor;
+    juce::ComboBox toneSelectionComboBox;
+    juce::Label toneSelectionLabel;
+    juce::ComboBox waveGroupComboBox;
+    juce::Label waveGroupLabel;
+    juce::Slider waveformSlider;
+    juce::Label waveformLabel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EditToneTab)
 };
